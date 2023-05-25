c7cb7c56634de8b808779f9da653a62b43a480fd
Thiago Odilon <thiago-almeida05@hotmail.com>
2023-05-25 12:35:31 -0300

    fix errors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart'
+import { useState } from 'react';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler(){
+    setCartIsShown(true);
+  }
+  function hideCartHandler(){
+    setCartIsShown(false);
+  }
   return (
     <CartProvider>
-      <Cart />
+      {cartIsShown && <div>Cart...</div>}
+      <Cart  onShowCart={showCartHandler}/>
       <Header />
       <main>
         <Meals />
